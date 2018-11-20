6e2839596874aafb2c8eec3bf6b5a64cdadfc058
Travis T <travistokuyama@gmail.com>
2018-11-20 22:45:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,12 @@ class App < Sinatra::Base
   end
 
   get '/' do
-    @session = session
     erb :index
+  end
+
+  post "/checkout" do
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
   end
 end
