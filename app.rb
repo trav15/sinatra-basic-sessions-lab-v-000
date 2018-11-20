4d2d279d0cb3f1b41b13623081e9bb62de6adfbe
Travis T <travistokuyama@gmail.com>
2018-11-20 22:38:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "hamburger"
+  end
+
+  get '/' do
+    @session = session
+    erb :index
+  end
 end
